2cfeda207cf328dcfd4dcdf936b513fb13ccaab6
Gustavo Felipe <110072296+guufelipe@users.noreply.github.com>
2024-12-12 14:58:55 -0300

    Add files via upload

---
+++ Desafio2.py
@@ -0,0 +1,20 @@
+class node:
+
+    def __init__(self, val, noDireita,noEsquerda, noPai):
+        self.pai = noPai
+        self.val = val
+        self.noDireita = noDireita
+        self.noEsquerda = noEsquerda
+
+    #while  (fh != 2 and fh != -2): Arvore balanceada; criar uma variavel booleana pra armazenar se a arvore está balanceada ou não.
+
+class AVL: 
+    def __init__(self):
+        
+        
+
+    def inserir():
+
+        
+    
+
